9a5fddf23f12bc5037780924b15af6fcf3125680
Asem-Badr <70919962+Asem-Badr@users.noreply.github.com>
2020-10-16 18:57:33 +0200

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+void createStack(Stack mystack)
+{
+    mystack.top = 0;
+}
+void push (Stack*ps,stack_elements element)
+{
+    ps->entry[ps->top++] = element;
+}
+void pop(Stack*ps,stack_elements* element)
+{
+    *element = ps->entry[ps->--top];
+}
+void clearStack(Stack*ps)
+{
+    ps->top = 0;
+}
+int stackEmpty(Stack *ps)
+{
+    if(ps->top==0)
+        return 1;
+    else
+        return 0;
+}
+int	stackFull(Stack *ps)
+{
+    if(ps->top == stack_size)
+        return 1;
+    else
+        return 0;
+}
+void stackTop(stack_elements *element, Stack *ps)
+{
+    *element = ps->entry[ps->top-1];
+}
+int	 stackSize(Stack *ps)
+{
+    /** returns the number of elements on a stack **/
+    return ps->top ;
+}
+
---
+++ stack.h
@@ -0,0 +1,24 @@
+#ifndef STACK_H
+#define STACK_H
+
+#define stack_size 100
+typedef int stack_elements  ;
+
+typedef struct stack1
+{
+    int top;
+    stack_elements entry[stack_size];
+
+} Stack;
+
+void createStack(Stack mystack);
+void push (Stack*ps,stack_elements element);
+void pop(Stack*ps,stack_elements* element);
+void clearStack(Stack*ps);
+int stackEmpty(Stack*ps);
+int	stackFull(Stack*ps);
+void stackTop(stack_elements *element, Stack *ps);
+int	 stackSize(Stack *ps);
+
+
+#endif
